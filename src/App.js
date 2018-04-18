d74c3214123e1ddbf94aac1bc99dc708faa0e4a5
vrrodgers <trulymissvicky1982@gmail.com>
2018-04-17 21:21:29 -0500

    add state and props chkp8

--- src/App.js
+++ src/App.js
@@ -1,20 +1,28 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
+import ToDo from "./components/ToDo.js";
+
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+     this.state = { 
+      todos: [
+        { description: "Walk the cat", isCompleted: true },
+        { description: "Throw the dishes away", isCompleted: false },
+        { description: "Buy new dishes", isCompleted: false }
+      ] 
+    };
+  }
   render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
-    );
+    return <div className="App">
+        <ul>
+          {this.state.todos.map((todo, index) => (
+            <ToDo key={ index } description={ todo.description } isCompleted={ todo.isCompleted }
+            />
+          ))}
+        </ul>
+      </div>;
   }
 }
 
